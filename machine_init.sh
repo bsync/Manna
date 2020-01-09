334fcd9f3cfa5b8a791071e8195d35dfd4c97d8b
Travis <james.horine@gmail.com>
2020-01-09 03:36:29 +0000

    Adding machine init script for convenience.

---
+++ machine_init.sh
@@ -0,0 +1,9 @@
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+sudo apt install -y docker-ce
+sudo usermod -aG docker ${USER}
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
