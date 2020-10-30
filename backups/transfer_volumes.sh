048c76479f05b1de6f3db4e31ef30fdaa7b32e8b
Travis Horine <james.horine@gmail.com>
2020-10-30 18:16:52 -0500

    Fix for transfer volumes script.

--- backups/transfer_volumes.sh
+++ backups/transfer_volumes.sh
@@ -2,15 +2,15 @@
 PROSERVER="ubuntu@tullahomabiblechurch.org"
 DEVSERVER="ubuntu@dev.tullahomabiblechurch.org"
 
-VOLUMES="manna_joomla_html manna_mongodb_data manna_mysql_data manna_nginx_secrets"
+VOLUMES="manna_static_html manna_joomla_html manna_mongodb_data manna_mysql_data manna_nginx_secrets"
+#VOLUMES="manna_joomla_html"
 for volume in $VOLUMES; do
    echo "Dumping $volume from $PROSERVER"
+   set -x
    ssh -i ~/Desktop/lightsail.pem  $PROSERVER \
-      set -x
-      docker run --rm -v $volume:/from \
-         alpine ash -c "cd /from ; tar -czf - . " \
-            > $volume.pro.tar.gz
-      set +x
+      docker run --rm -v $volume:/from alpine ash -c "cd /from ; tar -czf - ." \
+   > $volume.pro.tar.gz
+   set +x
 done
 
 #TODO: Upload volumes to DEVSERVER
