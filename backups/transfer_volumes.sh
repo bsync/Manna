07cd0ff4be73ea447c50a33faf2e3d06454c57ac
James T. Horine <james.horine@gmail.com>
2022-06-09 06:49:54 -0500

    add import functionality

--- backups/transfer_volumes.sh
+++ backups/transfer_volumes.sh
@@ -2,9 +2,9 @@
 PROSERVER="root@tullahomabiblechurch.org"
 DEVSERVER="root@tullahomabiblechurch.org"
 
-VOLUMES="manna_joomla_html manna_mongodb_data manna_mysql_data manna_static_html"
+VOLUMES="manna_joomla_html manna_mysql_data manna_static_html"
 
-while getopts "d:s:" arg; do
+while getopts "d:s:i:" arg; do
    case $arg in
       d )
         SERVER=${!OPTARG}
@@ -17,6 +17,12 @@ while getopts "d:s:" arg; do
 	for volume in $VOLUMES; do
       	   echo "Pushing $volume to $SERVER"
 	   cat $volume.tar.gz | ssh $SERVER 'docker run --rm -i -v ' $volume:/to 'alpine ash -c "cd /to ; tar -xzpvf - "'
+        done ;;
+      i )
+        SERVER=${!OPTARG}
+	for volume in $VOLUMES; do
+      	   echo "Importing $volume"
+	   cat $volume.tar.gz | docker run --rm -i -v ' $volume:/to 'alpine ash -c "cd /to ; tar -xzpvf - "
         done ;;
    esac
 done
